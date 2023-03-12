7ce9c7183bde3edfa1d0e558bc25494b17a72d28
bilalhodzic1 <bilalh.work0@gmail.com>
2023-03-12 18:25:56 -0500

    Javascript changes

--- weaponry_fetch.js
+++ weaponry_fetch.js
@@ -13,15 +13,73 @@ function appendData(data) {
   for (let product_type in data) {
     if (product_type == `Weapons`) {
       for (let element of data[product_type]) {
-        let div = document.createElement("div");
-        div.className = `catalog_item`;
-        div.innerHTML = `<img class="product_img" src="${element["imgLink"]}" alt="${element["product_name"]}" />
+        if (
+          localStorage.getItem(`firearm`) &&
+          element["category"] == `Firearm`
+        ) {
+          let div = document.createElement("div");
+          div.className = `catalog_item`;
+          div.innerHTML = `<img class="product_img" src="${element["imgLink"]}" alt="${element["product_name"]}" />
         <h2 class="product_name">${element["productName"]}</h2>
         <p class="product_description">${element["Description"]}</p>
-        <p class="product_price">${element["priceList"]}</p>`;
-        console.log(mainContainer);
-        mainContainer.appendChild(div);
+        <p class="product_price">$${element["priceList"]}</p>`;
+          console.log(mainContainer);
+          mainContainer.appendChild(div);
+        }
+        if (
+          localStorage.getItem(`melee`) &&
+          element["category"] == `Close Combat`
+        ) {
+          let div = document.createElement("div");
+          div.className = `catalog_item`;
+          div.innerHTML = `<img class="product_img" src="${element["imgLink"]}" alt="${element["product_name"]}" />
+        <h2 class="product_name">${element["productName"]}</h2>
+        <p class="product_description">${element["Description"]}</p>
+        <p class="product_price">$${element["priceList"]}</p>`;
+          console.log(mainContainer);
+          mainContainer.appendChild(div);
+        }
+        if (
+          localStorage.getItem(`explosive`) &&
+          element["category"] == `Explosives`
+        ) {
+          let div = document.createElement("div");
+          div.className = `catalog_item`;
+          div.innerHTML = `<img class="product_img" src="${element["imgLink"]}" alt="${element["product_name"]}" />
+        <h2 class="product_name">${element["productName"]}</h2>
+        <p class="product_description">${element["Description"]}</p>
+        <p class="product_price">$${element["priceList"]}</p>`;
+          console.log(mainContainer);
+          mainContainer.appendChild(div);
+        }
+        if (
+          element["id"] == data[product_type].length &&
+          localStorage.getItem(`firearm`)
+        ) {
+          localStorage.removeItem(`firearm`);
+        }
+        if (
+          element["id"] == data[product_type].length &&
+          localStorage.getItem(`meleet`)
+        ) {
+          localStorage.removeItem(`melee`);
+        }
+        if (
+          element["id"] == data[product_type].length &&
+          localStorage.getItem(`explosive`)
+        ) {
+          localStorage.removeItem(`explosive`);
+        }
       }
     }
   }
+}
+function storeSmtn() {
+  let firearmTrue = document.getElementById("firearm_selector");
+  let meleeTrue = document.getElementById("close_combat_selector");
+  let explosiveTrue = document.getElementById("explosive_selector");
+  localStorage.setItem(`firearm`, firearmTrue.checked);
+  localStorage.setItem(`melee`, meleeTrue.checked);
+  localStorage.setItem(`explosive`, explosiveTrue.checked);
+  console.log("HEllo");
 }
