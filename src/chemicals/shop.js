5277e02387abb861682a7832ebce73c78e35c9eb
bilalhodzic1 <bilalh.work0@gmail.com>
2023-04-12 18:30:50 -0500

    Displays items

--- src/chemicals/shop.js
+++ src/chemicals/shop.js
@@ -1,23 +1,53 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import items from "./chemicals.json";
 const ChemicalShop = () => {
   const [cart, setCart] = useState([]);
   const [filter, setFilter] = useState("");
-  const [products, setProducts] = useState([
-    { id: "", title: "", price: "", description: "", imgLink: "" },
-  ]);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    try {
-      const response = await fetch("./chemicals.json");
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.log("error", error);
-    }
+  const [products, setProducts] = useState([]);
+  const addToCart = (product) => {
+    setCart([...cart, product]);
   };
-  return <div></div>;
+  const removeFromCart = (product) => {
+    let hardCopy = [...cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== product.id);
+    setCart(hardCopy);
+  };
+  const listItems = items.map((product) => (
+    <div key={product.id}>
+      <div>
+        <div>
+          <img src={product.imgLink} />
+        </div>
+        <div>
+          <div>{product.title}</div>
+          <div>{product.description}</div>
+        </div>
+        <div>
+          <button
+            type="button"
+            variant="light"
+            onClick={() => removeFromCart(product)}
+          >
+            {" "}
+            -{" "}
+          </button>{" "}
+          <button
+            type="button"
+            variant="light"
+            onClick={() => addToCart(product)}
+          >
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  ));
+  return (
+    <div>
+      <div>{listItems}</div>
+    </div>
+  );
 };
 export default ChemicalShop;
