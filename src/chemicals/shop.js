e2a0dc34f06d8205bcea8b46302dde232fbc286b
Tristan <sayasit7@iastate.edu>
2023-04-12 18:32:55 -0500

    shop.js changes

--- src/chemicals/shop.js
+++ src/chemicals/shop.js
@@ -18,6 +18,34 @@ const ChemicalShop = () => {
       console.log("error", error);
     }
   };
-  return <div></div>;
+  return (
+    <div>
+      <h1>Saya's Chemicals</h1>
+      <div class="topnav">
+        <a class="active" href="#home">
+          Home
+        </a>
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search"></input>
+            <button type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+      </div>
+    </div>
+  );
 };
 export default ChemicalShop;
